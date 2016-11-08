2c573d7a4dabd8ee387b177bec766d4abb0cb72c
George Eaton <george_eaton@hotmail.co.uk>
2016-11-08 18:20:59 +0000

    added docking functionality

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,15 @@ describe DockingStation do
   it { is_expected.to respond_to ("release_bike") }
 
   it 'makes sure that the bike is working' do
-  bike = subject.release_bike
-  expect(bike.working?)
+    bike = subject.release_bike
+    expect(bike.working?)
+  end
+
+  it 'docks bikes' do
+    expect(subject.dock_bike('bmx')).to eq 'bmx'
+  end
+
+  it 'shows bike name' do
+    expect(subject.bike).to eq @bike
   end
 end
