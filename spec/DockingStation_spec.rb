4dc1cff3da21e0c96f0e6f497d43b0e8c44ab5c6
George Eaton <george_eaton@hotmail.co.uk>
2016-11-08 12:03:41 +0000

    added DockingStation class

--- spec/DockingStation_spec.rb
+++
@@ -1,4 +0,0 @@
-describe DockingStation do
-it "releases a bike to user" do
-end
-end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,6 @@
+require 'docking_station'
+
+describe DockingStation do
+  it "releases a bike to user" do
+  end
+end
